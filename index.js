ffd1d02612622b3b75c4c7770c677d45459058c9
Gaio B. Oliveira <gaio.casabranca@gmail.com>
2024-09-16 11:53:17 -0300

    Primeiro commit do node-api

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const axios =  require('axios')
+const { application } = require('express')
+const app = express()
+const porta = 3000
+
+//Rota padrão
+app.get('/', async(req,res)=>{
+    res.json({mensagem: "Bem-vindo a API"})
+})
+
+//Rota para dados
+app.get('/cidadesbr', async (req,res)=>{
+    try{
+        //Exemplo de API que retorna dados sobre cidades
+        const resposta = await axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/municipios')
+        res.json(resposta.data)
+    }
+    catch{(erro) =>
+        res.status(500).send('Erro ao buscar os dados!')
+    }
+})
+
+app.listen(porta, () => {
+
+    console.log(`Servidor rodando na porta ${porta}`)
+})
+ 
